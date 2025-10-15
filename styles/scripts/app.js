4b60276c008283ebf20cc34e7a352b56bf4787b5
Abdull-Kudus <a.zakariam@alustudent.com>
2025-10-15 06:50:56 +0200

    feat: add init function

--- styles/scripts/app.js
+++ styles/scripts/app.js
@@ -5,3 +5,23 @@ import * as Storage from './storage.js';
 import { validateForm, setupLiveValidation } from './validators.js';
 import { searchRecords, highlightMatches } from './search.js';
 
+// DOM Elements
+let elements = {};
+
+/**
+ * Initialize the application
+ */
+function init() {
+    // Initialize state
+    State.initState();
+    
+    // Cache DOM elements
+    cacheElements();
+    
+    // Setup event listeners
+    setupEventListeners();
+    
+    // Initial render
+    renderCurrentPage();
+}
+
