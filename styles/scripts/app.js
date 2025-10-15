ae25c49f4fbc701455200b67a2f783390f07b218
Abdull-Kudus <a.zakariam@alustudent.com>
2025-10-15 06:50:04 +0200

    chore: imports to be done

--- styles/scripts/app.js
+++ styles/scripts/app.js
@@ -1,2 +1,7 @@
 // MAIN APPLICATION JAVASCRIPT FILE
 
+import * as State from './state.js';
+import * as Storage from './storage.js';
+import { validateForm, setupLiveValidation } from './validators.js';
+import { searchRecords, highlightMatches } from './search.js';
+
